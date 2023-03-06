d67d331e386d68a0a6771113c5eb47e642327b41
VariosInstey <124655869+VariosInstey@users.noreply.github.com>
2023-03-06 23:23:50 +0300

    Добавьте файлы проекта.

---
+++ RDA.Task.4/View/MainWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using RDA.Task._4.Core;
+using RDA.Task._4.View.Pages;
+using RDA.Task._4.View.Pages.TaskPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RDA.Task._4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CoreNavigate.MyConnection = MainFrame;
+            MainFrame.Navigate(new MainPage());
+        }
+        private void Move_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+        private void PackIcon_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2 && this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+                Rectangle.Width = 2000;
+                Rectangle1.Width = 200;
+                Rectangle.Height = 150;
+                Rectangle1.Height = 1100;
+                Text1.FontSize = 24;
+                Text.FontSize = 24;
+                PackIcon.Height = 60;
+                PackIcon.Width = 60;
+                PackIcon1.Width = 60;
+                PackIcon1.Height = 60;
+                MainFrame.Height = 1000;
+                MainFrame.Width= 1800;
+            }
+            else if (e.ClickCount == 2 && this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+                Text1.FontSize = 12;
+                Text.FontSize = 12;
+                Rectangle.Width = 800;
+                Rectangle1.Width = 125;
+                Rectangle.Height = 100;
+                Rectangle1.Height = 450;
+                PackIcon.Height = 30;
+                PackIcon.Width = 30;
+                PackIcon1.Height = 30;
+                PackIcon1.Width = 30;
+                MainFrame.Height= 450;
+                MainFrame.Width = 800;
+            }
+        }
+        private void BtnTask1_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.MyConnection?.Navigate(new Task1Page());
+        }
+        private void BtnTask2_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.MyConnection?.Navigate(new Task2Page());
+        }
+        private void BtnTask3_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.MyConnection?.Navigate(new Task3Page());
+        }
+        private void BtnTask4_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.MyConnection?.Navigate(new Task4Page());
+        }
+        private void BtnTask5_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.MyConnection?.Navigate(new Task5Page());
+        }
+        private void BtnTask6_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.MyConnection?.Navigate(new Task6Page());
+        }
+        private void BtnTask7_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.MyConnection?.Navigate(new Task7Page());
+        }
+        private void BtnTask8_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.MyConnection?.Navigate(new Task8Page());
+        }
+        private void BtnTask9_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.MyConnection?.Navigate(new Task9Page());
+        }
+        private void BtnTask10_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.MyConnection?.Navigate(new Task10Page());
+        }
+        private void BtnTask11_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.MyConnection?.Navigate(new Task11Page());
+        }
+        private void BtnTask12_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.MyConnection?.Navigate(new Task12Page());
+        }
+        private void BtnTask13_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.MyConnection?.Navigate(new Task13Page());
+        }
+        private void BtnTask14_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.MyConnection?.Navigate(new Task14Page());
+        }
+        private void PackIcon1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            CoreNavigate.MyConnection?.Navigate(new MainPage());
+        }
+    }
+}
